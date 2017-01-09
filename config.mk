3cea0f946a43954ab74d56645ec80d52dde92d6e
Claudio Alessi <smoppy@gmail.com>
2017-01-09 17:10:17 +0100

    Fix a deprecated constant.

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ INCS = `mysql_config --cflags`
 LIBS = -lmysqlclient -lstfl -lncursesw
 
 # flags
-CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
+CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -g -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 #CFLAGS  = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
